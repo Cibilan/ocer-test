99a94b65a14c134592dfc8d4b9ba116bb3909459
Cibilan <cibilan@gmail.com>
2020-07-23 10:48:21 +0530

    basic setup done

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-var msg = 'Hello World';
-console.log(msg);
+
+const express = require('express')
+
+const tf = require('@tensorflow/tfjs')
+const automl = require('@tensorflow/tfjs-automl')
+
+//require('@tensorflow/tfjs-node')
+
+const modelUrl = require('./resources/model.json'); // URL to the model.json file.
+
+const app = express();
+
+
+app.use('/useAutoml', async (req,res)=> {
+
+    //const model = await automl.loadObjectDetection(modelUrl);
+
+    res.send("hellpo");
+
+})
+
+app.use((req,res,next)=>{
+    res.send("page not found");
+})
+ 
+
+
+app.listen(3000);
+
+
+
